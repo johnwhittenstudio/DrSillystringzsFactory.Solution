2ab8c2bca248a3acffe36b5c7da6cd43e3702d10
John Whitten <johnwhitten.studio@gmail.com>
2022-03-24 14:27:56 -0700

    complete Machine model

---
+++ Factory/Models/Machine.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+
+namespace Factory.Models
+{
+  public class Machine
+  {
+    public Machine()
+    {
+      this.JoinEntities = new HashSet<EngineerMachine>();
+    }
+
+    public int MachineId { get; set; }
+    public string MachineType { get; set; }
+    public string MachineDetails { get; set; }
+    public virtual ICollection<EngineerMachine> JoinEntities { get; set; }
+  }
+}
